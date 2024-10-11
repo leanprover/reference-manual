df7482b173438da157b149c4a658ca59b921014b
David Thrane Christiansen <david@davidchristiansen.dk>
2024-10-11 11:18:03 +0200

    fix: always build figures in CI
    Work around issue with a blunt hammer...

---
+++ figures.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# FIXME: The Lake config doesn't seem to always build the figures. For now, this is a hack to work
+# around it, but it should be fixed when deadlines are over.
+
+shopt -s nullglob
+
+cd figures || exit
+for f in *.tex; do
+    stem=${f%.*}
+    lualatex "$stem"
+    lualatex "$stem"
+    lualatex "$stem"
+    lualatex "$stem"
+    pdftocairo -svg "$stem.pdf" "$stem.svg"
+done
+
+cd ..
+cp figures/*.pdf static/figures/
+cp figures/*.svg static/figures/
