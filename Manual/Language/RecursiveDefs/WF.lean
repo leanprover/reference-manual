7e9e792d8ff62f40d3c90461504d6a8fce44251c
Joachim Breitner <mail@joachim-breitner.de>
2025-01-14 14:00:57 +0100

    Apply suggestions from code review
    Co-authored-by: David Thrane Christiansen <david@davidchristiansen.dk>

--- Manual/Language/RecursiveDefs/WF.lean
+++ Manual/Language/RecursiveDefs/WF.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Lean FRO LLC. All rights reserved.
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: David Thrane Christiansen
+Author: Joachim Breitner
 -/
 
 import VersoManual
