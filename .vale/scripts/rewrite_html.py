f82ca8f7c890e24c57226f23af6b239f7eff9ebb
David Thrane Christiansen <david@lean-fro.org>
2025-02-13 21:57:11 +0100

    chore: add syntax to left-hand ToC (#304)

--- .vale/scripts/rewrite_html.py
+++ .vale/scripts/rewrite_html.py
@@ -50,6 +50,10 @@ def process_html_file(filepath, output_filepath):
     for element in soup.find_all(class_="no-vale"):
         element.decompose()
 
+    # Delete ignored elements
+    for element in soup.find_all(class_="split-toc"):
+        element.decompose()
+
     # Replace citations with their text
     for element in soup.find_all(class_="citation"):
         for inner in element.contents:
