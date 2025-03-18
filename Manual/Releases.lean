22ebb668a440cdc0797f1664819cd6e511b0f4a7
Joachim Breitner <mail@joachim-breitner.de>
2025-03-18 11:02:50 +0100

    feat: add release notes (#350)
    This includes the release notes in the reference manual, see
    https://pr-350--lean-reference-manual-review.netlify.app/releases/v4.17.0/
    for a preview.
    
    It uses Verso’s markdown parser so that one does not have to manually
    edit the text for verso-compatibility (our commit messages are messy).
    
    It uses one file and page per release, for easy linking and that it
    doesn’t grow too large, and (in particular) that you can scroll or
    Ctrl-F without losing track of which version this relates to.
    
    Co-authored-by: David Thrane Christiansen <david@lean-fro.org>

---
+++ Manual/Releases.lean
@@ -0,0 +1,94 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Joachim Breitner
+-/
+
+import VersoManual
+
+import Manual.Releases.«v4.17.0»
+import Manual.Releases.«v4.16.0»
+import Manual.Releases.«v4.15.0»
+import Manual.Releases.«v4.14.0»
+import Manual.Releases.«v4.13.0»
+import Manual.Releases.«v4.12.0»
+import Manual.Releases.«v4.11.0»
+import Manual.Releases.«v4.10.0»
+import Manual.Releases.«v4.9.0»
+import Manual.Releases.«v4.8.0»
+import Manual.Releases.«v4.7.0»
+import Manual.Releases.«v4.6.0»
+import Manual.Releases.«v4.5.0»
+import Manual.Releases.«v4.4.0»
+import Manual.Releases.«v4.3.0»
+import Manual.Releases.«v4.2.0»
+import Manual.Releases.«v4.1.0»
+import Manual.Releases.«v4.0.0»
+import Manual.Releases.«v4.0.0-m5»
+import Manual.Releases.«v4.0.0-m4»
+import Manual.Releases.«v4.0.0-m3»
+import Manual.Releases.«v4.0.0-m2»
+import Manual.Releases.«v4.0.0-m1»
+
+open Manual
+open Verso.Genre
+
+#doc (Manual) "Release Notes" =>
+%%%
+tag := "release-notes"
+file := "releases"
+number := false
+%%%
+
+This section provides release notes about recent versions of Lean. When updating to a new version, please
+read the corresponding release notes. They may contain advice that will help you understand
+the differences with the previous version and upgrade your projects.
+
+The work-in-progress release notes of a release candidate can be found [in the Lean GitHub
+repository](https://github.com/leanprover/lean4/tree/master/releases).
+
+{include 0 Manual.Releases.«v4.17.0»}
+
+{include 0 Manual.Releases.«v4.16.0»}
+
+{include 0 Manual.Releases.«v4.15.0»}
+
+{include 0 Manual.Releases.«v4.14.0»}
+
+{include 0 Manual.Releases.«v4.13.0»}
+
+{include 0 Manual.Releases.«v4.12.0»}
+
+{include 0 Manual.Releases.«v4.11.0»}
+
+{include 0 Manual.Releases.«v4.10.0»}
+
+{include 0 Manual.Releases.«v4.9.0»}
+
+{include 0 Manual.Releases.«v4.8.0»}
+
+{include 0 Manual.Releases.«v4.7.0»}
+
+{include 0 Manual.Releases.«v4.6.0»}
+
+{include 0 Manual.Releases.«v4.5.0»}
+
+{include 0 Manual.Releases.«v4.4.0»}
+
+{include 0 Manual.Releases.«v4.3.0»}
+
+{include 0 Manual.Releases.«v4.2.0»}
+
+{include 0 Manual.Releases.«v4.1.0»}
+
+{include 0 Manual.Releases.«v4.0.0»}
+
+{include 0 Manual.Releases.«v4.0.0-m5»}
+
+{include 0 Manual.Releases.«v4.0.0-m4»}
+
+{include 0 Manual.Releases.«v4.0.0-m3»}
+
+{include 0 Manual.Releases.«v4.0.0-m2»}
+
+{include 0 Manual.Releases.«v4.0.0-m1»}
