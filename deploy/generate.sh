4e3b8dd58d9bda7900ebab0423ca11b4442b3528
David Thrane Christiansen <david@davidchristiansen.dk>
2025-03-21 20:53:06 +0100

    fixes after local tests

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-lake --quiet exe generate-manual --depth 2 --with-word-count "words.txt" --verbose --without-html-single --output "html"
+lake --quiet exe generate-manual --depth 2 --verbose --without-html-single --output "html"
