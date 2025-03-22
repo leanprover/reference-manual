205926bb6e39887693d2499b1f417acb1ce1bb2b
David Thrane Christiansen <david@lean-fro.org>
2025-03-22 17:23:52 +0100

    perf: speed up incremental builds by splitting List into modules (#379)

---
+++ Manual/BasicTypes/List/Modification.lean
@@ -0,0 +1,85 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
+import VersoManual
+
+import Manual.Meta
+
+open Manual.FFIDocType
+
+open Verso.Genre Manual
+
+set_option pp.rawOnError true
+
+#doc (Manual) "Modification" =>
+
+{docstring List.set}
+
+{docstring List.setTR}
+
+{docstring List.modify}
+
+{docstring List.modifyTR}
+
+{docstring List.modifyHead}
+
+{docstring List.modifyTailIdx}
+
+{docstring List.erase}
+
+{docstring List.eraseTR}
+
+{docstring List.eraseDups}
+
+{docstring List.eraseIdx}
+
+{docstring List.eraseIdxTR}
+
+{docstring List.eraseP}
+
+{docstring List.erasePTR}
+
+{docstring List.eraseReps}
+
+{docstring List.extract}
+
+{docstring List.removeAll}
+
+{docstring List.replace}
+
+{docstring List.replaceTR}
+
+{docstring List.reverse}
+
+{docstring List.flatten}
+
+{docstring List.flattenTR}
+
+{docstring List.rotateLeft}
+
+{docstring List.rotateRight}
+
+{docstring List.leftpad}
+
+{docstring List.leftpadTR}
+
+{docstring List.rightpad}
+
+# Insertion
+
+{docstring List.insert}
+
+{docstring List.insertIdx}
+
+{docstring List.insertIdxTR}
+
+{docstring List.intersperse}
+
+{docstring List.intersperseTR}
+
+{docstring List.intercalate}
+
+{docstring List.intercalateTR}
