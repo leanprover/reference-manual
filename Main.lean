fc71f53fc84c66ffb511d5ca8b3076b836251625
David Thrane Christiansen <david@lean-fro.org>
2025-05-23 14:14:42 +0200

    chore: bump to latest nightly (2025-05-23), elan, and Verso (#448)
    Verso now ships KaTeX itself, so no need to include it here.

--- Main.lean
+++ Main.lean
@@ -23,44 +23,6 @@ def plausible := {{
 
 
 
-def KaTeXLicense : LicenseInfo where
-  identifier := "MIT"
-  dependency := "KaTeX"
-  howUsed := "KaTeX is used to render mathematical notation."
-  link := "https://katex.org/"
-  text := #[(some "The MIT License", text)]
-where
-  text := r#"
-Copyright (c) 2013-2020 Khan Academy and other contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"#
-
-
-def addKaTeX (config : Config) : Config :=
-  {config with
-    extraCss := "/static/katex/katex.min.css" :: config.extraCss,
-    extraJs := "/static/katex/katex.min.js" :: "/static/math.js" :: config.extraJs,
-    licenseInfo := KaTeXLicense :: config.licenseInfo
-  }
-
-
 def fuzzysortLicense : LicenseInfo where
   identifier := "MIT"
   dependency := "fuzzysort v3.1.0"
@@ -119,7 +81,7 @@ def scarfPixel := {{
 def main :=
   manualMain (%doc Manual) (config := config)
 where
-  config := addKaTeX {
+  config := Config.addKaTeX {
     extraFiles := [("static", "static")],
     extraCss := [
       "/static/colors.css",
