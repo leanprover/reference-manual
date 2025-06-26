f70d9a9a09d66fb4e72fbac119dfd1d13aaadf44
David Thrane Christiansen <david@davidchristiansen.dk>
2025-06-26 09:10:51 +0200

    header fix

--- Manual/Meta/CheckMessages.lean
+++ Manual/Meta/CheckMessages.lean
@@ -1,4 +1,10 @@
-import Lean
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
+import Lean.Elab.GuardMsgs
 import SubVerso.Examples.Messages
 open Lean Elab Command
 
