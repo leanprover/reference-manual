078f007b32fe65f51ebd91cf091be1bc233ca565
Kim Morrison <kim@tqft.net>
2025-07-01 09:31:46 +1000

    chore: release notes for v4.22.0
    chore: release notes for v4.22.0

--- Manual/Releases.lean
+++ Manual/Releases.lean
@@ -6,6 +6,7 @@ Author: Joachim Breitner
 
 import VersoManual
 
+import Manual.Releases.«v4_22_0»
 import Manual.Releases.«v4_21_0»
 import Manual.Releases.«v4_20_1»
 import Manual.Releases.«v4_20_0»
@@ -48,6 +49,8 @@ number := false
 This section provides release notes about recent versions of Lean. When updating to a new version, please
 read the corresponding release notes. They may contain advice that will help you understand
 the differences with the previous version and upgrade your projects.
+
+{include 0 Manual.Releases.«v4_22_0»}
 
 {include 0 Manual.Releases.«v4_21_0»}
 
