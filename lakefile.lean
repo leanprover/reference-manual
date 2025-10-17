1db7b625d4c4f2aa6d42e32c6a3e1907b565463b
Rob Simmons <rob@lean-fro.org>
2025-10-16 20:33:01 -0400

    fix: temporarily point release to a branch commit in verso

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ import Lake
 open Lake DSL
 open System (FilePath)
 
-require verso from git "https://github.com/leanprover/verso.git"@"5a9318d"
+require verso from git "https://github.com/leanprover/verso.git"@"73bbf11"
 
 package "verso-manual" where
   -- building the C code cost much more than the optimizations save
