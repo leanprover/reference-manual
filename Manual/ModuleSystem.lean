b3717b13329807513bcf0d1748f424931fb929a9
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-03 10:34:33 +0100

    Apply suggestions from code review
    Co-authored-by: David Thrane Christiansen <david@lean-fro.org>

--- Manual/ModuleSystem.lean
+++ Manual/ModuleSystem.lean
@@ -128,11 +128,11 @@ When it comes to actual code execution, there is no point to a definition withou
 Thus, in order to eagerly know what definitions _might_ be executed at compile time and so need to be available including their bodies (in some executable shape), any definition used as an entry point to compile-time execution has to be tagged with the new `meta` modifier.
 This is automatically done in built-in metaprogramming syntax such as `syntax`, `macro`, and `elab` but may need to be done explicitly when manually applying metaprogramming attributes such as `@[app_delab]`.
 
-A `meta` definition may access (and thus invoke) other `meta` definitions only; a non-`meta` definition likewise may only access other non-`meta` definitions.
+A `meta` definition may only access (and thus invoke) other `meta` definitions; a non-`meta` definition likewise may only access other non-`meta` definitions.
 For imported definitions, the `meta` marker can be added after the fact using `meta import`.
 `meta import`ing a definition already in the meta phase leaves it in that phase.
 In addition, the import must be public if the imported definition may be compile-time executed outside the current module, i.e. if it is reachable from some public `meta` definition in the current module: use `public meta import` or, if already `meta`, `public import`.
-This is usually the case, unless a definition was imported solely for use in `local` metaprograms.
+This is usually the case, unless a definition was imported solely for use in local metaprograms such as `local syntax/macro/elab/...`.
 ```
 module
 
@@ -146,9 +146,11 @@ local elab "my_elab" : command => do
 For convenience, `meta` also implies `partial`.
 This can be overridden by giving an explicit `termination_by` metric (such as one suggested by `termination_by?`), which may be necessary when the type of the definition is not known to be `Nonempty`.
 
-As a guideline, it is usually preferable to apply `meta` as late as possible.
-Thus helper definitions not directly registered as metaprograms do not need to be marked if they are located in a separate module.
-Only the final module(s) actually registering metaprograms then should use `public meta import` to import those helpers and define its metaprograms using built-in syntax like `elab`, using `meta def`, or using `meta section`.
+As a guideline, it is usually preferable to keep the amount of `meta` annotations as small as possible.
+This avoids locking otherwise-reusable declarations into the meta phase and it helps the build system avoid more rebuilds.
+Thus, when a metaprogram depends on other code that does not itself need to be marked `meta`, this other code should be located in a separate module and not marked `meta`.
+Only the final module that actually registers a metaprogram needs the helpers as `meta` definitions.
+It should use `public meta import` to import those helpers and then define its metaprograms using built-in syntax like `elab`, using `meta def`, or using `meta section`.
 
 # Common Errors and Patterns
 
