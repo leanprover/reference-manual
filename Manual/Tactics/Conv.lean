353fd3d6fb32c82af207fba7ffc6afe98eafa3ad
Wojciech Rozowski <wojciech@lean-fro.org>
2026-02-05 16:39:57 +0000

    chore: fix lean output

--- Manual/Tactics/Conv.lean
+++ Manual/Tactics/Conv.lean
@@ -222,7 +222,12 @@ termination_by (n,0)
 example : f 5 = 5 := by rfl
 ```
 ```leanOutput nonEq
-...
+Tactic `rfl` failed: The left-hand side
+     f 5
+   is not definitionally equal to the right-hand side
+     5
+
+   ⊢ f 5 = 5
 ```
 Using {conv}`cbv` on the left-hand side of the equality, the statement can be made true:
 ```lean
