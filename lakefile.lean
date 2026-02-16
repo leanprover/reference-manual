1d9a1d113d47889b3577aba507bd454f17361f8a
Kim Morrison <kim@tqft.net>
2026-02-17 01:14:10 +1100

    chore: bump toolchain to v4.28.0

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ import Lake
 open Lake DSL
 open System (FilePath)
 
-require verso from git "https://github.com/leanprover/verso.git"@"v4.28.0-rc1"
+require verso from git "https://github.com/leanprover/verso.git"@"v4.28.0"
 require versowebcomponents from git "https://github.com/leanprover/verso-web-components"@"main"
 
 package "verso-manual" where
