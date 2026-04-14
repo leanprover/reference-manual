72f23189d41a8c90c11575a35a288ed775649e16
Joscha <joscha@plugh.de>
2026-04-14 17:02:31 +0200

    doc: add release notes for v4.29.1 and v4.28.1

---
+++ Manual/Releases/v4_28_1.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2026 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Joscha Mennicken
+-/
+
+import VersoManual
+import Manual.Meta
+import Manual.Meta.Markdown
+
+open Manual
+open Verso.Genre
+open Verso.Genre.Manual
+open Verso.Genre.Manual.InlineLean
+
+#doc (Manual) "Lean 4.28.1 (2026-04-14)" =>
+%%%
+tag := "release-v4.28.1"
+file := "v4.28.1"
+%%%
+
+```markdown
+
+- [#13392](https://github.com/leanprover/lean4/pull/13392)
+  fixes a heap buffer overflow in `lean_io_prim_handle_read` that was triggered through an
+  integer overflow in the size computation of an allocation. In addition it places several checked
+  arithmetic operations on all relevant allocation paths to have potential future overflows be turned
+  into crashes instead. The offending code now throws an out of memory error instead.
+
+```
