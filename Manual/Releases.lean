c1c21f66c582857dc9308c95238cf184aaf7bac0
bigmac0 <impetus1@protonmail.com>
2026-05-18 10:55:51 -0400

    this reflects only highlight changes

--- Manual/Releases.lean
+++ Manual/Releases.lean
@@ -61,7 +61,6 @@ This section provides release notes about recent versions of Lean. When updating
 read the corresponding release notes. They may contain advice that will help you understand
 the differences with the previous version and upgrade your projects.
 
-
 {include 0 Manual.Releases.«v4_30_0»}
 
 {include 0 Manual.Releases.«v4_29_1»}
