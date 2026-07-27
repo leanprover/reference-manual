f99b6ccf8b4cb81e1364cabcbd50ec13868c3af2
Julien Cretin <git@ia0.eu>
2026-07-27 14:09:28 +0200

    doc: remove unneeded schema variable for axiom K reduction rule (#901)

--- Manual/BasicProps.lean
+++ Manual/BasicProps.lean
@@ -372,8 +372,7 @@ def K {α : Sort u}
 
 example {α : Sort u} {a : α}
     {motive : {x : α} → x = x → Sort u}
-    {d : {x : α} → motive (Eq.refl x)}
-    {v : motive (Eq.refl a)} :
+    {d : {x : α} → motive (Eq.refl x)} :
     K (motive := motive) d a rfl = d := by
   rfl
 ```
