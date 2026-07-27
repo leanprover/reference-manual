3a554e32e1ef85f25b072baa1f73c285562727bc
Julien Cretin <git@ia0.eu>
2026-07-27 14:08:35 +0200

    doc: fix typo in the documentation of the syntax for cast (#900)

--- Manual/BasicProps.lean
+++ Manual/BasicProps.lean
@@ -340,7 +340,7 @@ Propositional equality is typically denoted by the infix `=` operator.
 $_ ▸ $_
 ```
 When a term's type includes one side of an equality as a sub-term, it can be rewritten using the `▸` operator.
-If the both sides of the equality occur in the term's type, then the left side is rewritten to the right.
+If both sides of the equality occur in the term's type, then the left side is rewritten to the right.
 :::
 
 ## Uniqueness of Equality Proofs
