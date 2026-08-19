b2a4b0dd8ba30bc3dbe1c7ccd0fa4032a2390170
David Thrane Christiansen <david@davidchristiansen.dk>
2026-08-19 21:13:41 +0200

    chore: bump to nightly-2026-08-18

--- Manual/BasicTypes/Option.lean
+++ Manual/BasicTypes/Option.lean
@@ -127,8 +127,8 @@ In {lean}`getAlpha`, a line of input is read.
 If the line consists only of letters (after removing whitespace from the beginning and end of it), then it is returned; otherwise, the function returns {name}`none`.
 
 ```lean
-def getAlpha : IO (Option String) := do
-  let line := (← (← IO.getStdin).getLine).trim
+def getAlpha : IO (Option String.Slice) := do
+  let line := (← (← IO.getStdin).getLine).trimAscii
   if line.length > 0 && line.all Char.isAlpha then
     return line
   else
